1abd40b2569254dae1d585005377eda00d32103a
Josue Martinez <josue.mart@jmcsoluciones.com>
2022-09-10 16:44:25 -0500

    Ultimos cambios para la actividad 2 de la unidad 3

--- index.php
+++ index.php
@@ -3,34 +3,32 @@ include('header.php')
 ?>
 <section class="slides">
     <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
-    <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    </div>
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-        <img src="./assets/images/slide1-1920x466.png" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block textSlide">
-            <h5>Conoce nuestro Instituto</h5>
-            <a href="acerca.php">Más Información</a>
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
         </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="./assets/images/slide1-1920x466.png" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block textSlide">
+                    <h5>Conoce nuestro Instituto</h5>
+                    <a href="acerca.php">Más Información</a>
+                </div>
+             </div>
+            <div class="carousel-item">
+                <img src="./assets/images/slide2-1920x466.png" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block textSlide">
+                <h5>Departamentos Disponibles</h5>
+                <a href="departamentos.php">Más Información</a>  
+            </div>
         </div>
         <div class="carousel-item">
-        <img src="./assets/images/slide2-1920x466.png" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block textSlide">
-            <h5>Departamentos Disponibles</h5>
-            <a href="departamentos.php">Más Información</a>
-            
-        </div>
-        </div>
-        <div class="carousel-item">
-        <img src="./assets/images/slide4-1920x466.png" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block textSlide">
-            <h5>Nuestros servicios</h5>
-            <a href="servicios.php">Más Información</a>
-
-        </div>
+            <img src="./assets/images/slide4-1920x466.png" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block textSlide">
+                <h5>Nuestros servicios</h5>
+                <a href="servicios.php">Más Información</a>
+            </div>
         </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
